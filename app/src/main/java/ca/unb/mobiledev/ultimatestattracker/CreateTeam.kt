3949cda29a44cb24aac924f27fdd2c412f05d98e
j-savoie <jacob.savoie@ymail.com>
2022-10-23 15:45:43 -0300

    Add Button and layouts for team create

---
+++ app/src/main/java/ca/unb/mobiledev/ultimatestattracker/CreateTeam.kt
@@ -0,0 +1,11 @@
+package ca.unb.mobiledev.ultimatestattracker
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class CreateTeam : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.create_team)
+    }
+}
--- app/src/main/java/ca/unb/mobiledev/ultimatestattracker/MainActivity.kt
+++ app/src/main/java/ca/unb/mobiledev/ultimatestattracker/MainActivity.kt
@@ -1,11 +1,20 @@
 package ca.unb.mobiledev.ultimatestattracker
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    private var button : Button = findViewById<Button>(R.id.button)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val intent = Intent(this, CreateTeam::class.java)
+
+        button.setOnClickListener {
+            startActivity(intent)
+        }
     }
+
 }
