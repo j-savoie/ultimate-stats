5d117b91379d9cb71670b9b55deadeb148d8d833
Ben Fleming <bflemin1@unb.ca>
2022-10-25 16:42:24 -0300

    Fix Warnings

--- app/src/main/java/ca/unb/mobiledev/ultimatestattracker/AddPlayers.kt
+++ app/src/main/java/ca/unb/mobiledev/ultimatestattracker/AddPlayers.kt
@@ -10,7 +10,6 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import ca.unb.mobiledev.ultimatestattracker.model.Team
 import ca.unb.mobiledev.ultimatestattracker.model.Player
-import kotlin.math.ln
 
 class AddPlayers : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,26 +19,26 @@ class AddPlayers : AppCompatActivity() {
         val lnameField = findViewById<EditText>(R.id.editTextLname)
         val numberField = findViewById<EditText>(R.id.editTextNumber)
         val genderSpinner = findViewById<Spinner>(R.id.spinner)
-        val button : Button = findViewById<Button>(R.id.AddPlayerButton)
-        val completeRoster : Button = findViewById<Button>(R.id.completeRoster)
-        var team : Team = intent.getSerializableExtra("AddPlayers") as Team
-        val genders = arrayOf<String>("Male", "Female", "Other")
+        val button : Button = findViewById(R.id.AddPlayerButton)
+        val completeRoster : Button = findViewById(R.id.completeRoster)
+        val team : Team = intent.getSerializableExtra("AddPlayers") as Team
+        val genders = arrayOf("Male", "Female", "Other")
         val adapter : ArrayAdapter<String> = ArrayAdapter(this, android.R.layout.simple_spinner_item, genders)
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         genderSpinner.adapter = adapter
 
         button.setOnClickListener{
-            var fname = fNameField.text.toString()
-            var lname = lnameField.text.toString()
-            var number = numberField.text.toString().toInt()
-            var genderString = genderSpinner.selectedItem.toString()
+            val fname = fNameField.text.toString()
+            val lname = lnameField.text.toString()
+            val number = numberField.text.toString().toInt()
+            val genderString = genderSpinner.selectedItem.toString()
             var gender : Player.GENDER = Player.GENDER.Male
             when(genderString){
                 "Male" -> gender = Player.GENDER.Male
                 "Female" -> gender = Player.GENDER.Female
                 "Other" -> gender = Player.GENDER.Other
             }
-            var player = Player(fname,lname,number, gender)
+            val player = Player(fname,lname,number, gender)
             team.players.add(player)
             Toast.makeText(this, "Player Added: $fname $lname", Toast.LENGTH_SHORT).show()
             fNameField.setText("")
@@ -48,7 +47,7 @@ class AddPlayers : AppCompatActivity() {
         }
         completeRoster.setOnClickListener{
             Log.e("team: ", team.teamName)
-            Log.e("player: ", team.players.get(0).fName)
+            Log.e("player: ", team.players[0].fName)
         }
     }
 }
