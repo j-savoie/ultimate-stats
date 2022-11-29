56780558eb4fae8cbfb9806c8671696e46d7ef08
j-savoie <jacob.savoie@ymail.com>
2022-11-29 13:17:48 -0400

    add StatTracker page

---
+++ app/src/main/java/ca/unb/mobiledev/ultimatestattracker/StatTracker.kt
@@ -0,0 +1,14 @@
+package ca.unb.mobiledev.ultimatestattracker
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class StatTracker : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        supportActionBar?.hide()
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.stat_tracker)
+
+    }
+}
